44b0dd1b8f0bf96e4fa643be2bf51e8d2fc69e01
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2019-02-14 20:30:56 -0700

    add api structure for brand signup (Task 42)

--- app/index.py
+++ app/index.py
@@ -6,6 +6,10 @@ influencers = [
   { 'name': 'john', 'email': 'john@example.com' }
 ]
 
+brands = [{'name' : 'Brand1', 'email' : 'brand1@email.com'}, 
+		  {'name' : 'Brand2', 'email' : 'brand2@email.com'}, 
+]
+
 @app.route('/influencers')
 def get_influencers():
   return jsonify(influencers)
@@ -15,3 +19,14 @@ def signup():
   influencers.append(request.get_json())
   #write to database table  - influeners
   return '', 204
+  
+@app.route('/brands')
+def get_brands():
+  return jsonify(brands)
+
+@app.route('/brands', methods=['POST'])
+def signup():
+  influencers.append(request.get_json())
+  #write to database table  - influeners
+  return '', 204
+  
