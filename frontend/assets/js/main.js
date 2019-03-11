39917405fec8c74daa5aa512359b9c0dbf5b8855
M.SOWMYA <smadabh1@asu.edu>
2019-03-11 10:13:53 -0700

    Popup Form Trial

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -266,3 +266,11 @@
 	});
 
 })(jQuery);
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
