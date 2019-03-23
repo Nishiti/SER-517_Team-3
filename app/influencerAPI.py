94e63e0e6e3d08dea8f09b72cb6a0f4a2a0343e0
hshibani <mercury26th@gmail.com>
2019-03-22 17:57:30 -0700

    move influencersignup API to its own class and register the class with a route

---
+++ app/influencerAPI.py
@@ -0,0 +1,43 @@
+from flask_restful import Resource
+from flask import request, app
+from flask_restful.representations import json
+from app.models import Influencer
+
+class InfluencerSignUpAPI(Resource):
+    def post(self):
+        data = request.get_json()
+        if Influencer.objects(email=data['email']):
+            data = {
+                "role": "influencer",
+                "message": "Influencer already exists in database!"
+            }
+            response = app.response_class(
+                response=json.dumps(data),
+                status=409,
+                mimetype='application/json'
+            )
+        else:
+            influencer = Influencer(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                category=data['category'],
+                youtube=data['youtube'],
+                IGStoryViews=data['IGStoryViews'],
+                followers=data['followers'],
+                AvgLikes=data['AvgLikes'],
+                AvgComments=data['AvgComments'],
+                Gender=data['Gender'],
+                email=data['email'],
+                website=data['website'],
+                social_media_handles=data['instagram_handle']
+            ).save()
+            data = {
+                "role": "influencer",
+                "message": "Influencer added in database!"
+            }
+            response = app.response_class(
+                response=json.dumps(data),
+                status=201,
+                mimetype='application/json'
+            )
+        return response
