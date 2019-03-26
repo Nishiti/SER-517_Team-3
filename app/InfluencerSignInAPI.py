cc8ae03f1a56d9f8d77e18f8d159ed18c9d66d10
vbhatt1 <vbhatt1@asu.edu>
2019-03-26 11:53:29 -0700

    Code added to compare log in credentials

--- app/InfluencerSignInAPI.py
+++ app/InfluencerSignInAPI.py
@@ -7,10 +7,27 @@ app = Flask(__name__)
 app.secret_key = 'vihar'
 api = Api(app)
 
+users = [
+	{
+		'email':'bob',
+		'password':'abc'
+	}
+]
+
 class InfluencerSignIn(Resource):
 
 	def post(self):
 
+	data = InfluencerSignIn.parser.parse_args()
+
+	for user in users:
+		if user['email'] == data['email']:
+			if user['password'] == data['password']:
+				return {"message":"Login Successfull."}
+			else:
+				return {"message":"The email id and password combination is invalid."}
+		else:
+			{"message":"The given username doesn't exist."}
 
 
 api.add_resource(InfluencerSignIn, '/influencersignin')
