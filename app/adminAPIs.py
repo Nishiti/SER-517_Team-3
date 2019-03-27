0f0c89fefca77fa90b7c6bab5b69a44e7897fb67
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2019-03-26 21:01:17 -0700

    refactor code for Influencer model file (Task 129)

--- app/adminAPIs.py
+++ app/adminAPIs.py
@@ -31,6 +31,19 @@ class AdminDeactivateBrandAPI(Resource):
             return make_response(jsonify(role='admin', message='brand has been deactivated'), status.HTTP_200_OK)
 
 
+class AdminRemoveInfluencerAPI(Resource):
+    def post(self):
+        data = request.get_json(force=True)
+        if not Influencer.objects(email=data['email']):
+            return make_response(jsonify(role='admin', message='brand does not exist in database'),
+                                 status.HTTP_404_NOT_FOUND)
+        else:
+            brand = Brand.objects(email=data['email']).first()
+            brand.delete()
+            return make_response(jsonify(role='brand', message='brand has been removed from database'),
+                                 status.HTTP_200_OK)
+
+
 class AdminApproveBrandSignupAPI(Resource):
     def post(self):
         data = request.get_json(force=True)
