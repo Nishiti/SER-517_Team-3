c26d23b7e6cffcd221990a95e92e96154e95d2d9
nishitisawant <nnsawant@asu.edu>
2019-03-29 00:46:01 -0700

    #159 Adding angular elements to the influencer's signup

--- frontend/assets/js/influencerData.js
+++ frontend/assets/js/influencerData.js
@@ -17,3 +17,60 @@ app.directive("matchPassword", function () {
         }
     };
 });
+
+app.controller('myCtrl', function($scope,$http) {
+    //console.log($scope.cname)
+
+    $scope.update = function() {
+        console.log($scope.fname);
+        console.log($scope.lname);
+        console.log($scope.email);
+        console.log($scope.password);
+        console.log($scope.confirm_password);
+        console.log($scope.cname);
+        console.log($scope.cname);
+        console.log($scope.cname);
+        console.log($scope.cname);
+
+        var data =
+        {
+            "company_name" : $scope.cname,
+            "email" : $scope.email,
+            "password" : $scope.num,
+            "address" : "tempe"
+        }
+
+
+
+        $http.post('http://localhost:5000/brand', data).then(function (response) {
+
+            if (response.data)
+
+                $scope.msg = "Post Data Submitted Successfully!";
+
+        }, function (response) {
+
+            $scope.msg = "Service not Exists";
+
+            $scope.statusval = response.status;
+
+            $scope.statustext = response.statusText;
+
+            $scope.headers = response.headers();
+
+        });
+
+    };
+
+});
+
+
+
+
+
+    //    .then(function(response) {  //Success
+
+          //  $scope.content = response.data;
+        //}, function(response) {     //Failure
+        //    $scope.content = "Something went wrong";
+        //});
