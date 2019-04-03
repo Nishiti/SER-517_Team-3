90f11dad8ede5a29e3c3902e63ffb37b4bf5ddb5
vbhatt1 <vbhatt1@asu.edu>
2019-04-03 11:33:58 -0700

    Created a model for brand campaign (Task 149)

---
+++ app/BrandCampaign.py
@@ -0,0 +1,9 @@
+from mongoengine import *
+
+class BrandCampaign(Document):
+	campaign_name = StringField(max_length=50, required=True)
+	email = EmailField(required=True)
+	gift_campaign = BooleanField(null=False, default=False)
+	gift_code = BooleanField(null=False, default=False)
+	website_social_media_handles = StringField(max_length=150)
+	campaign_information_requirements = StringField(max_length=500, required=True)
