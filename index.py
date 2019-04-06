6ecb9d236b78b3bc234b1cf72e37dc9199a8e0c3
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2019-04-06 14:55:23 -0700

    refactor code (Task 153)

--- index.py
+++ index.py
@@ -27,99 +27,7 @@ login_manager.init_app(app)
 
 connect('ser517', host='mongodb://localhost/ser517')
 
-# @app.route('/influencers')
-# def get_influencers():
-#     influencers = Influencer.objects()
-#     if influencers is None:
-#         data = {
-#         "role": "influencer",
-#         "message": "No influencer data found in database!"
-#         }
-#         response = app.response_class(
-#         response=json.dumps(data),
-#         status=401,
-#         mimetype='application/json'
-#         )
-#     else:
-#         '''data = {
-#             "role": "influencer",
-#             "message": "Influencer data is found in database!"
-#         }'''
-#         response = app.response_class(
-#         response=json.dumps(influencers),
-#         status=200,
-#         mimetype='application/json'
-#         )
-#     return response
 
-# @app.route('/brand/signup', methods=['POST'])
-# def signupbrands():
-#   data = (request.get_json())
-#   if Brand.objects(email=data['email']):
-#     data = {
-#       "role": "brand",
-#       "message": "brand already exists in database"
-#     }
-#     response = app.response_class(
-#       response=json.dumps(data),
-#       status=409,
-#       mimetype='application/json'
-#     )
-#
-#   else:
-#     user = Brand(
-#       first_name=data['first_name'],
-#       last_name = data['last_name'],
-#       email = data['email'],
-#       password = data['password'],
-#       website = data['website']
-#     ).save()
-#
-#     data = {
-#       "role": "brand",
-#       "message": "new brand is added in database"
-#     }
-#     response = app.response_class(
-#       response=json.dumps(data),
-#       status=201,
-#       mimetype='application/json'
-#     )
-#
-#   return response
-
-# @app.route('/brand/update', methods=['POST'])
-# def updatebrands():
-#   data = (request.get_json())
-#   # handle case when email is not found
-#   brand = Brand.objects(email=data['email'])
-#   if not brand:
-#     data = {
-#       "role": "brand",
-#       "message": "brand does not exists in database"
-#     }
-#     response = app.response_class(
-#       response=json.dumps(data),
-#       status=404,
-#       mimetype='application/json'
-#     )
-#   else:
-#     brand = brand[0]
-#     data = request.get_json()
-#     for key in data:
-#       brand[key] = data[key]
-#     brand.save()
-#
-#     data = {
-#       "role": "brand",
-#       "message": "brand updated in database"
-#     }
-#     response = app.response_class(
-#       response=json.dumps(data),
-#       status=200,
-#       mimetype='application/json'
-#     )
-#
-#   return response
 
 @app.route('/brand/signin', methods=['POST'])
 def signinbrands():
@@ -158,47 +66,7 @@ def signinbrands():
       )
   return response
 
-# @app.route('/admin/approve/brandsingup', methods=['POST'])
-# def admin_approve_brand_singup():
-#   data = (request.get_json())
-#   if not Brand.objects(email=data['email']):
-#     data = {
-#       "role": "brand",
-#       "message": "brand is not found in database"
-#     }
-#     response = app.response_class(
-#       response=json.dumps(data),
-#       status=404,
-#       mimetype='application/json'
-#     )
-#
-#   else:
-#     brand = Brand.objects(email=data['email'])
-#     brand = brand[0]
-#     if brand['isapproved'] == False:
-#       brand.isapproved = True
-#       brand.isactive = True
-#       brand.save()
-#       data = {
-#         "role": "admin",
-#         "message": "brand is approved and updated in database"
-#       }
-#       response = app.response_class(
-#         response=json.dumps(data),
-#         status=200,
-#         mimetype='application/json'
-#       )
-#     else:
-#       data = {
-#         "role": "admin",
-#         "message": "brand is already approved in database"
-#       }
-#       response = app.response_class(
-#         response=json.dumps(data),
-#         status=200,
-#         mimetype='application/json'
-#       )
-#   return response
+
 
 
 api.add_resource(BrandAPI, '/brand')
