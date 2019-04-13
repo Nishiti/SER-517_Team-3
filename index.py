b4526065dc717dba9db21907af2460abe0b91d7b
hshibani <mercury26th@gmail.com>
2019-04-12 18:26:07 -0700

    Task #225 - Admin logout by revoking json web tokens

--- index.py
+++ index.py
@@ -22,6 +22,8 @@ from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import os
 
+from nxstlab.revokedtoken import RevokedToken
+
 app = Flask(__name__)
 api = Api(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -43,6 +45,11 @@ connect(config['dbname'], host=config['host'])
 def hello():
     return send_file('templates/index.html')
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return RevokedToken.is_jti_blacklisted(jti)
+
 api.add_resource(BrandAPI, '/brand')
 api.add_resource(AdminSignInAPI, '/admin/signin')
 api.add_resource(AdminSignupAPI, '/admin/signup')
@@ -54,7 +61,9 @@ api.add_resource(AdminGetInfluencersWithFilterAPI, '/admin/getInfluencers')
 api.add_resource(InfluencerSignUpAPI, '/influencer/signup')
 api.add_resource(BrandCampaignRequestAPI, '/brandcampaignrequest')
 api.add_resource(BrandCampaignRequestApproveAPI, '/brandcampaignrequestapprove')
-#api.add_resource(AdminSignoutAPI, '/admin/signout')
+# api.add_resource(AdminSignoutAPI, '/admin/signout')
+api.add_resource(AdminSignoutAPI, '/admin/signoutaccess')
+api.add_resource(AdminSignoutAPI, '/admin/signoutrefresh')
 
 # Test Resource
 api.add_resource(SecretResource, '/admin/secret')
@@ -62,6 +71,9 @@ api.add_resource(SecretResource, '/admin/secret')
 if __name__ == '__main__':
     app.secret_key = os.urandom(16)
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 60
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
     jwt = JWTManager(app)
     app.debug = True
     app.run(port=5000)
---
+++ nxstlab/revokedtoken.py
@@ -0,0 +1,24 @@
+from mongoengine import Document
+from mongoengine import StringField, IntField
+
+from nxstlab.admin import Admin
+
+
+class RevokedToken(Document):
+    id = IntField(primary_key=True)
+    jti = StringField(max_length=120)
+
+    '''def add(self):
+        print('add')'''
+
+    @classmethod
+    def is_jti_blacklisted(jti):
+        query = Admin.objects(jti=jti).first()
+        print('query = ', query)
+        return bool(query)
+
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+        query = cls.query.filter_by(jti=jti).first()
+        print('type of query= ', type(query))
+        return bool(query)
