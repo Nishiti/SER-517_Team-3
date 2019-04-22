d0ee83292f6f68432acfff95cec357506a7003eb
hshibani <mercury26th@gmail.com>
2019-04-22 14:36:38 -0700

    Task #240 Image upload functionality for influencer profile

--- nxstlab/InfluencerUpdateProfile.py
+++ nxstlab/InfluencerUpdateProfile.py
@@ -1,3 +1,5 @@
+from flask_restful.representations import json
+
 from nxstlab.models import Influencer
 from flask_restful import Resource
 from flask import jsonify, request, make_response
@@ -14,11 +16,33 @@ class InfluencerUpdateProfile(Resource):
             data = request.get_json()
             for key in data:
                 if key == 'image':
-                    imagefile = request.files['imagefile.jpg']
+                    file = request.files['image']
+                    # extension = os.path.splitext(file.filename)[1]
                     #influencer['image'] = imagefile.read()
-                    influencer.image.put(imagefile)
+                    influencer.image.put(file)
                 else:
                     influencer[key] = data[key]
             influencer.save()
-            return make_response(jsonify(role='infuencer', message='Influencer details updated successfully in database'),
+            return make_response(jsonify(role='influencer', message='Influencer details updated successfully in database'),
                                  status.HTTP_200_OK)
+
+
+    def get(self, id):
+        influencers = [influencer for influencer in Influencer.objects()]
+        res = []
+        for influencer in influencers:
+            temp = dict()
+            temp['first_name'] = influencer.first_name
+            temp['last_name'] = influencer.last_name
+            temp['email'] = influencer.email
+            temp['website_social_media_handles'] = influencer.website_social_media_handles
+            temp['followers'] = influencer.followers
+            temp['areas_of_interest'] = influencer.areas_of_interest
+            temp['image'] = influencer.image
+            print('image retreived')
+            # temp['image'] = json.loads(temp['image'].data.decode('utf-8'))
+            temp['image'] = temp['image'].data.decode('utf-8')
+            print('image decoded')
+            res.append(temp)
+        return make_response(jsonify(data=res, role='influencer', message='Influencer profile details'),
+                             status.HTTP_200_OK)
