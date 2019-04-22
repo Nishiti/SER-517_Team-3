8663711a8aad78a4ff2277fbbeac4c5c24e914f2
vbhatt1 <vbhatt1@asu.edu>
2019-04-21 21:52:04 -0700

    Created test case for influencer sign up (Task 197)

---
+++ test_influencer.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+import unittest
+import index
+import json
+from mongoengine import connect
+from flask_mongoengine import MongoEngine
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# index.app.config['TESTING'] = True
+# index.app.config['MONGO_DBNAME'] = 'ser517'
+# index.app.config['MONGO_URI'] = 'mongodb://localhost/ser517'
+# index.app.config['DEBUG'] = True
+
+class ProjectTestCase(unittest.TestCase):
+
+	def setUp(self):
+		connect('test_ser517', host = 'mongodb://localhost/test_ser517')
+		index.app.config['TESTING'] = True
+		index.app.config["MONGODB_DB"] = 'test_ser517'
+		self.app = index.app.test_client()
+
+
+	def test_influencer_signup(self):
+
+		#tests the addition of new user to the database
+		res = self.app.post('/influencer/signup', data = json.dumps({"first_name":"first",
+													"last_name":"first",
+													"email":"zux@mail.com",
+													"password":"abcdefgh",
+													"confirm_password":"abcdefgh"}), content_type = 'application/json')
+
+		self.assertEqual(res.status_code, 201)
+		
+		#tests addition of an already existing user
+		res = self.app.post('/influencer/signup', data = json.dumps({"first_name":"first",
+													"last_name":"first",
+													"email":"zux@mail.com",
+													"password":"abcdefgh",
+													"confirm_password":"abcdefgh"}), content_type = 'application/json')
+		self.assertEqual(res.status_code, 409)
+
+if __name__ == "__main__":
+    unittest.main()
