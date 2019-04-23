d898bbdef4b0f98af59c9c66cc0b1c1e66cfe365
vbhatt1 <vbhatt1@asu.edu>
2019-04-22 22:59:35 -0700

    Test cases to test brand sign up (Task 192)

---
+++ test_brands.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+import unittest
+import index
+import json
+from mongoengine import connect
+from flask_mongoengine import MongoEngine
+
+class TestBrands(unittest.TestCase):
+
+	def setUp(self):
+		connect('test_ser517', host = 'mongodb://localhost/test_ser517')
+		index.app.config['TESTING'] = True
+		index.app.config["MONGODB_DB"] = 'test_ser517'
+		self.app = index.app.test_client()
+
+
+	def test_signup_correct(self):
+
+		#tests the addition of new user to the database
+		res = self.app.post('/brand', data = json.dumps({"company_name":"ruffles",
+													"address":"mumbai",
+													"email":"ruffles@mail.com",
+													"password":"abc12345",
+													"confirm_password":"abc12345"}), content_type = 'application/json')
+
+		self.assertEqual(res.status_code, 201)
+		
+	def test_signup_incorrect(self):
+
+		#tests addition of an already existing user
+		res = self.app.post('/brand', data = json.dumps({"company_name":"ruffles",
+													"address":"mumbai",
+													"email":"ruffles@mail.com",
+													"password":"abc12345",
+													"confirm_password":"abc12345"}), content_type = 'application/json')
+		self.assertEqual(res.status_code, 409)
+
+if __name__ == "__main__":
+    unittest.main()
