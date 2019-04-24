18c8f5487529b5d552391db141f20ea38fd9fa1f
vbhatt1 <vbhatt1@asu.edu>
2019-04-24 00:25:41 -0700

    Separate methods for easier execution of test cases (Task 192, 195, 197)

--- test_usersignin.py
+++ test_usersignin.py
@@ -14,28 +14,29 @@ class TestUserSignin(unittest.TestCase):
 		index.app.config["MONGODB_DB"] = 'test_ser517'
 		self.app = index.app.test_client()
 
-
+#test signin using correct email and password
 	def test_signin_correct(self):
 
-		#test signin using correct email and password
 		res = self.app.post('/user/signin', data = json.dumps({
 													"email":"zux@mail.com",
 													"password":"abcdefgh"}), content_type = 'application/json')
 
 		self.assertEqual(res.status_code, 200)
 		
+
+#tests signin using incorrect password
 	def test_signin_incorrect_password(self):
 
-		#tests signin using incorrect password
 		res = self.app.post('/user/signin', data = json.dumps({
 													"email":"zux@mail.com",
 													"password":"abcdefghi"
 													}), content_type = 'application/json')
 		self.assertEqual(res.status_code, 401)
 
+
+#tests signin using incorrect email
 	def test_signin_incorrect_email(self):
 
-		#tests signin using incorrect password
 		res = self.app.post('/user/signin', data = json.dumps({
 													"email":"bux@mail.com",
 													"password":"abcdefgh"
