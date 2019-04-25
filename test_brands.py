3a7f00a4e7e7617cd4060b666f8b4d14155bfa44
vbhatt1 <vbhatt1@asu.edu>
2019-04-25 14:32:49 -0700

    Created test cases that test the brand campaign request (Task 199)

--- test_brands.py
+++ test_brands.py
@@ -26,7 +26,7 @@ class TestBrands(unittest.TestCase):
 
 		self.assertEqual(res.status_code, 201)
 		
-#tests addition of an already existing user
+#tests addition of an already existing brand
 	def test_signup_incorrect(self):
 
 		res = self.app.post('/brand', data = json.dumps({"company_name":"ruffles",
@@ -35,6 +35,27 @@ class TestBrands(unittest.TestCase):
 													"password":"abc12345",
 													"confirm_password":"abc12345"}), content_type = 'application/json')
 		self.assertEqual(res.status_code, 409)
+
+#tests the brand campaign request which is not already in the database
+	def test_correct_brand_campaign_request(self):
+
+		res = self.app.post('/brandcampaignrequest', data = json.dumps({"campaign_name":"chips",
+													"email":"ruffles@mail.com",
+													"gift_campaign":False,
+													"gift_code":False,
+													"campaign_info_rqmts":"campaign for chips"}), content_type = 'application/json')
+		self.assertEqual(res.status_code, 201)
+
+#tests the brand campaign request which is already in the database
+	def test_incorrect_brand_campaign_request(self):
+
+		res = self.app.post('/brandcampaignrequest', data = json.dumps({"campaign_name":"chips",
+													"email":"ruffles@mail.com",
+													"gift_campaign":False,
+													"gift_code":False,
+													"campaign_info_rqmts":"campaign for chips"}), content_type = 'application/json')
+		self.assertEqual(res.status_code, 409)
+
 
 if __name__ == "__main__":
     unittest.main()
